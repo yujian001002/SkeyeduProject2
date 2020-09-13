c303e9806b310f2dad807999d699ea40d66d2b78
SuperYu <3412166951@qq.com>
2020-09-13 23:14:46 +0800

    java项目添加进来, 并设置好了忽略的文件

---
+++ maven_one/src/main/java/com/bus/servlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.bus.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("loginName", "张小三");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
